e8bad2bd3ddbcc7f3b8a451d6867862b40ddda43
liusheng <65622218@qq.com>
2019-05-27 18:20:18 +0800

    时间轴

--- packages/time-axis/src/TimeAxis.vue
+++ packages/time-axis/src/TimeAxis.vue
@@ -1,37 +1,21 @@
 <template>
     <div class = 'time-axis'>
-        <div class="time-item">
+        <div class="time-item" v-for="(item,index) in timeData" :key="index">
 			<div class="time-picture">
-				<img src="./svg.svg" alt="">
+				<img :src="item.imgUrl">
 			</div> 
 
-			<div class="time-content">
-				<h2>Title of section 1</h2>
-				<p>aa</p>
+			<div class="time-content" :style="color?getBgColor():''">
+				<h2>{{item.title}}</h2>
+				<p>{{item.content}}</p>
 				<div class="more-btn">
-                    <span class="date">2019/5/27</span>
+                    <span class="date">{{item.time}}</span>
                     <div class="a-btn">
-                        <a href="#0" class="read-more">详情</a>
+                        <a @click="fnJump(item.url)" class="read-more">详情</a>
                     </div>
                 </div>
 			</div> 
-		</div> 
-        <div class="time-item">
-			<div class="time-picture">
-				<img src="./svg.svg" alt="">
-			</div> 
-
-			<div class="time-content">
-				<h2>Title of section 1</h2>
-				<p>aa</p>
-				<div class="more-btn">
-                    <span class="date">2019/5/27</span>
-                    <div class="a-btn">
-                        <a href="#0" class="read-more">详情</a>
-                    </div>
-                </div>
-			</div> 
-		</div> 
+		</div>
     </div>
 </template>
 <script>
@@ -39,18 +23,68 @@ export default {
     name:'TimeAxis',
     data () {
         return {
+
         };
     },
-    props: {},
+    props: {
+        timeData:{
+            type:Array,
+            default:[
+                {
+                    imgUrl:"http://pic.58pic.com/58pic/11/71/64/90h58PICfG2.jpg",
+                    title:"TimeAxis",
+                    content:"TimeAxis content",
+                    url:"#",
+                    time:"2019/5/27"
+                },
+                {
+                    imgUrl:"http://pic.58pic.com/58pic/11/71/64/90h58PICfG2.jpg",
+                    title:"TimeAxis",
+                    content:"TimeAxis content",
+                    url:"#",
+                    time:"2019/5/27"
+                },
+                {
+                    imgUrl:"http://pic.58pic.com/58pic/11/71/64/90h58PICfG2.jpg",
+                    title:"TimeAxis",
+                    content:"TimeAxis content",
+                    url:"#",
+                    time:"2019/5/27"
+                },
+            ]
+        },
+        color:{
+            type:Boolean,
+            default:true
+        }
+    },
     components: {},
     computed: {},
-    created() {
-        console.log(this.getRandomColor())
-    },
+    created() {},
     mounted() {},
     methods: {
         getRandomColor(){
-            return '#'+Math.floor(Math.random()*0xffffff).toString(16);time-axistime-axis
+            let colorArr = [
+                "#f44336",
+                "#e91e63",
+                "#9c27b0",
+                "#03a9f4",
+                "#4caf50",
+                "#8bc34a",
+                "#cddc39",
+                "#ffeb3b",
+                "#ffc107",
+                "#ff9800",
+                "#ff5722"
+            ]
+            // return '#'+Math.floor(Math.random()*0xffffff).toString(16);
+            return colorArr[~~(Math.random()*colorArr.length)];
+        },
+        getBgColor(){
+            return `background:linear-gradient(${Math.random()*360}deg,${this.getRandomColor()},${this.getRandomColor()})`
+        },
+        fnJump(url){
+            this.$router.push(url)
         }
     },
     watch: {}
